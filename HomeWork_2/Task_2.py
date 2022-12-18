a42ca44bbdf395bf73434b59a1351206b24b7810
JuliaRyzhova <16-j.lazy@bk.ru>
2022-12-18 10:11:59 +0300

    Added decision Task 2

---
+++ HomeWork_2/Task_2.py
@@ -0,0 +1,11 @@
+# Требуется найти наименьший натуральный делитель целого числа N, отличный от 1.
+# Пример:
+# Для n = 15: Ответ: 3
+# Для n = 35: Ответ: 5
+
+number = int(input('Input any number: '))
+
+for i in range(2, number):
+    if number % i == 0:
+        print(f'The min divider of number {number} is {i}')
+        break
