f95f8dfaef1dff5b9e3b66fbf5e44267512c6cb6
JuliaRyzhova <16-j.lazy@bk.ru>
2022-12-21 14:54:03 +0300

    Fixed Task 1

---
+++ HomeWork_3/Task_1.py
@@ -0,0 +1,18 @@
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, 
+# стоящих на нечётной позиции.
+# Пример:
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+import random
+
+size_list = 10
+list_numbers = [size_list]
+for i in range(size_list):
+    list_numbers.append(random.randint(0, 20))
+
+sum_odd_item = 0
+for i in range(1, size_list, 2):
+    sum_odd_item += list_numbers[i]
+
+print(list_numbers)
+print(f'The sum of the odd items in list is {sum_odd_item}')
