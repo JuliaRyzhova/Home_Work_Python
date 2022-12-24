04dc456bb87ceeb54d1cd3c35becb89edfc23b95
JuliaRyzhova <16-j.lazy@bk.ru>
2022-12-24 18:51:55 +0300

    Fix: Task 2

---
+++ HomeWork_4/Task_2.py
@@ -0,0 +1,53 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+number = int(input('Input any number: '))
+
+# список для простых чисел
+simple_numbers_list = []
+# count- счетчик делителей
+count = 0
+# проходим циклом по всем числам от 2 до N. Т.к. 1 - это и не простое и не составное число
+for i in range(2, number + 1):
+    for j in range(2, i):
+        if i % j == 0:
+            count += 1
+    if count == 0:
+        simple_numbers_list.append(i)
+    else:
+        count = 0
+
+# print (simple_numbers_list)
+
+def decompose_number(value):
+    for i in simple_numbers_list:
+        while value % i == 0:
+            if value > i:
+                print(i, end =' * ')
+                value //= i 
+            else:
+                print(i)
+                break
+
+decompose_number(number)
+
+
+
+
+
+
+
+#     for (int i = startValue; i < endValue; i++)
+# {   
+#     if (i==2) sum+=i;
+#     for (int j = 2;j < i;j++)
+#     {
+#         if(i%j==0 && i%1 ==0)
+#         {
+#             break;
+#         }
+#         else if (i==j+1)
+#         {
+#             sum+=i;
+#         }
+#     }
+# }
