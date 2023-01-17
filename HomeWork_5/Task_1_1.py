e3487281787f3343215bf884848796ce430bb7a3
JuliaRyzhova <16-j.lazy@bk.ru>
2023-01-17 12:00:38 +0300

    fix task 1.1

--- HomeWork_5/Task_1_1.py
+++ HomeWork_5/Task_1_1.py
@@ -5,52 +5,72 @@
 
 # 120 21 ---> 99 бот 4 -> 95 .... бот --->29 --> 27 >> 2конф
 
-# a) Добавьте игру против бота
-
-# # Доп b) Подумайте как наделить бота ""интеллектом"" (Теория игр)
-
 from random import randint
 
-# Алгоритм игры Человек против человека.
-    
-gamer_1 = True
-gamer_2 = False
+# Алгоритм игры Человек против человека. Добавила жеребьевку первого хода
 
-bonbon = 120
-max_bon = 28
-min_bon = 1
+total_sweet = 120
+take_sweet = 0
+player1_sweet = 0
+player2_sweet = 0
+max_take = 28
+min_take = 1
 
-def step_of_play(candies, min_bon, max_bon):
-    if candies >= max_bon:
-        step = int(input(f'Вы можете взять от {min_bon} до {max_bon} конфет: '))
-        candies -= step
+# Функция жеребьевки первого хода
+
+
+def first_step():
+    rnd = randint(1, 2)
+    if rnd == 1:
+        player1_step()
     else:
-        step = int(input(f'Вы можете взять от {min_bon} до {candies} конфет: '))
-        candies -= step
-    return step
+        player2_step()
 
-def play(player_1, player_2, candies, function):
-    while candies > 0:
-        if player_1:
-            print('Ход первого игрока: ')
-            print(f'На столе {candies} конфет(ы)')
-            step = step_of_play(candies, min_bon, max_bon)
-            candies -= step
-            print()
-            player_1, player_2 = False, True
-        elif player_2:
-            print('Ход второго игрока: ')
-            print(f'На столе {candies} конфет(ы)')
-            step = step_of_play(candies, min_bon, max_bon)
-            candies -= step
-            print()
-            player_2, player_1 = False, True
+# Логика хода первого игрока
+
+
+def player1_step():
+    global total_sweet
+    global take_sweet
+    global player1_sweet
+    print(f'Ход первого игрока. Сейчас на столе {total_sweet} конфет(ы).')
+    take_sweet = int(input('Сколько берете конфет? '))
+    print()
+    while take_sweet > max_take or take_sweet < min_take or take_sweet > total_sweet:
+        take_sweet = int(
+            input('Неверное количество конфет. Попробуйте снова: '))
+    total_sweet -= take_sweet
+    player1_sweet += take_sweet
+    if total_sweet > 0:
+        player2_step()
     else:
-        if player_1:
-            print('Game Over. Победил игрок номер 2')
-        elif player_2:
-            print('Game Over. Победил игрок номер 1')
+        print('Вы победили!!!!!!!!!!')
+
+# Логика хода второго игрока
 
 
-play(gamer_1, gamer_2, bonbon, step_of_play)
+def player2_step():
+    global total_sweet
+    global take_sweet
+    global player2_sweet
+    print(f'Ход второго игрока. Сейчас на столе {total_sweet} конфет(ы)')
+    take_sweet = int(input('Сколько берете конфет? '))
+    print()
+    while take_sweet > max_take or take_sweet < min_take or take_sweet > total_sweet:
+        take_sweet = int(
+            input('Неверное количество конфет. Попробуйте снова: '))
+    total_sweet -= take_sweet
+    player2_sweet += take_sweet
+    if total_sweet > 0:
+        player1_step()
+    else:
+        print('Вы победили!!!!!!!!!')
 
+
+def main():
+    print('На столе лежит 120 конфета. Играют два игрока делая ход друг после друга.\nЗа один ход можно забрать не более чем 28 конфет. Победитель - тот, кто оставил на столе 0 конфет.')
+    print()
+    first_step()
+
+
+main()
