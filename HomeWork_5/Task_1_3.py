18646cecc403a8d2cdd68abf0c0f9ec9a94cdcb2
JuliaRyzhova <16-j.lazy@bk.ru>
2023-01-17 12:01:39 +0300

    fix task 1.3

--- HomeWork_5/Task_1_3.py
+++ HomeWork_5/Task_1_3.py
@@ -1,49 +1,69 @@
 #  Алгоритм игры Человек против умного бота.
+from random import randint
 
-gamer = True
-bot = False
+total_sweet = 120
+take_sweet = 0
+human_sweet = 0
+bot_sweet = 0
+max_take = 28
+min_take = 1
 
-bonbon = 120
-max_bon = 28
-min_bon = 1
+# Функция жеребьевки первого хода
 
-def step_of_human(candies, min_bon, max_bon):
-    if candies >= max_bon:
-        step = int(
-            input(f'Вы можете взять от {min_bon} до {max_bon} конфет: '))
-        candies -= step
+
+def first_step():
+    rnd = randint(1, 2)
+    if rnd == 1:
+        human_step()
     else:
-        step = int(
-            input(f'Вы можете взять от {min_bon} до {candies} конфет: '))
-        candies -= step
-    return step
+        bot_step()
 
-def play(player, bot, candies, func_1):
-    while candies > 0:
-        if player:
-            print('Ваш ход: ')
-            print(f'На столе {candies} конфет(ы)')
-            step = step_of_human(candies, min_bon, max_bon)
-            candies -= step
-            print()
-            player, bot = False, True
-        elif bot:
-            print('Ход бота: ')
-            if candies >= max_bon:
-                step = min_bon + max_bon - step
-                candies -= step
-            else:
-                step = candies
-                candies -= step
-            print(f'Бот взял {step} конфет(ы)')
-            print(f'Осталось {candies} конфет(ы)')
-            print()
-            bot, player = False, True
+# Логика хода первого игрока
+
+
+def human_step():
+    global total_sweet
+    global take_sweet
+    global human_sweet
+    print(f'Ваш ход. Сейчас на столе {total_sweet} конфет(ы).')
+    take_sweet = int(input('Сколько берете конфет? '))
+    print()
+    while take_sweet > max_take or take_sweet < min_take or take_sweet > total_sweet:
+        take_sweet = int(
+            input('Неверное количество конфет. Попробуйте снова: '))
+    total_sweet -= take_sweet
+    human_sweet += take_sweet
+    if total_sweet > 0:
+        bot_step()
     else:
-        if player == False:
-            print('Поздравляю! Вы победили')
-        elif bot == False:
-            print('Game Over. Победил бот')
+        print('Вы победили!!!!!!')
+
+# Логика хода бота. Если остаток от деления общего(остаточного) числа конфет не равен 0, 
+# то Бот будет брать этот остаток от деления. В ином случае - рандом
 
 
-play(gamer, bot, bonbon, step_of_human)
+def bot_step():
+    global total_sweet
+    global take_sweet
+    global bot_sweet
+    if total_sweet % (min_take + max_take) !=0:
+            take_sweet = total_sweet % (min_take + max_take)
+    else:
+        take_sweet = randint(min_take, max_take)
+    print(f'Бот взял {take_sweet} конфет(ы)')
+    print()
+    total_sweet -= take_sweet
+    bot_sweet += take_sweet
+    if total_sweet > 0:
+        human_step()
+    else:
+        print('Бот выйграл!!!!!!!')
+
+
+def main():
+    print('На столе лежит 120 конфета. Играют два игрока делая ход друг после друга.\nЗа один ход можно забрать не более чем 28 конфет. Победитель - тот, кто оставил на столе 0 конфет.')
+    print()
+    first_step()
+
+
+main()
